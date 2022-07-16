71a66cc5491d1e802b283a824cd98d0a54b3810c
1251776527 <93086282+1251776527@users.noreply.github.com>
2022-07-16 18:54:12 +0800

    Add files via upload

---
+++ Homework/BookLost.java
@@ -0,0 +1,41 @@
+package 蓝桥杯.Homework;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class BookLost extends Lost{
+    private String bookName;
+    private String lostPlace;
+
+    public BookLost(){}
+
+    public BookLost(String type,String bookName, String lostPlace, String lostTime) throws ParseException {
+        setType(type);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        setLostTime(sdf.parse(lostTime));
+        this.bookName = bookName;
+        this.lostPlace = lostPlace;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getLostPlace() {
+        return lostPlace;
+    }
+
+    public void setLostPlace(String lostPlace) {
+        this.lostPlace = lostPlace;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+        return "书籍类：" + bookName + '\t'  + lostPlace + '\t' + sdf.format(getLostTime());
+    }
+}
